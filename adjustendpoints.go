9ad038592b3c9b62c8a127c331f3c3635e95b375
Tim Krehan <Tim.Krehan@datev.de>
2025-10-23 16:36:54 +0200

    implement adjustendpointsHandler to process DNS record changes via POST requests

--- adjustendpoints.go
+++ adjustendpoints.go
@@ -1,9 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"external-dns-opnsense/opnsense"
+	"net/http"
+
+	"sigs.k8s.io/external-dns/endpoint"
+)
 
 // adjustendpointsHandler handles HTTP requests to apply changes to DNS records.
 // Only POST requests are allowed. It processes create and delete operations based on the request body.
 func adjustendpointsHandler(w http.ResponseWriter, r *http.Request) {
-	
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var endpoints []*endpoint.Endpoint
+	json.NewDecoder(r.Body).Decode(&endpoints)
+	adjustedEndpoints, err := AdjustEndpoints(api, endpoints)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/external.dns.webhook+json;version=1")
+	json.NewEncoder(w).Encode(adjustedEndpoints)
+}
+
+func AdjustEndpoints(api *opnsense.OpnSenseApi, endpoints []*endpoint.Endpoint) ([]*endpoint.Endpoint, error) {
+	var createdEndpoints []*endpoint.Endpoint
+	var error error
+
+	return createdEndpoints, error
 }
